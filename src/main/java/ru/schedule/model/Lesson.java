73113e6034e298c7419d8e774817012c959c315d
pavelrk97 <pavel_r_k97@mail.ru>
2026-02-16 21:57:52 +0300

    Protection models

--- src/main/java/ru/schedule/model/Lesson.java
+++ src/main/java/ru/schedule/model/Lesson.java
@@ -1,26 +1,61 @@
 package ru.schedule.model;
 
-import java.util.*;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class Lesson {
+@Entity
+@Table(name = "lessons")
+public class Lesson extends BaseEntity {
 
-    public int order;
+    private int orderNumber;
 
-    /** Название темы / предмета */
-    public String title;
+    private String title;
+    private String lecturer;
 
-    /** Основной преподаватель (если один) */
-    public String lecturer;
+    @ElementCollection
+    @CollectionTable(name = "lesson_lecturers", joinColumns = @JoinColumn(name = "lesson_id"))
+    @Column(name = "lecturer")
+    private List<String> lecturers = new ArrayList<>();
 
-    /** Дополнительные преподаватели (для контроля и кейсов) */
-    public List<String> lecturers = new ArrayList<>();
+    private int durationHours;
 
-    /** Длительность в часах */
-    public int durationHours;
+    @Enumerated(EnumType.STRING)
+    private LessonType type;
 
-    /** Тип занятия */
-    public LessonType type;
+    private String note;
 
-    /** Примечание (например: СП, Intermediate Examination) */
-    public String note;
+    public int getOrderNumber() { return orderNumber; }
+    public void setOrderNumber(int orderNumber) { this.orderNumber = orderNumber; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getLecturer() { return lecturer; }
+    public void setLecturer(String lecturer) { this.lecturer = lecturer; }
+
+    public List<String> getLecturers() { return lecturers; }
+    public void setLecturers(List<String> lecturers) { this.lecturers = lecturers; }
+
+    public int getDurationHours() { return durationHours; }
+    public void setDurationHours(int durationHours) { this.durationHours = durationHours; }
+
+    public LessonType getType() { return type; }
+    public void setType(LessonType type) { this.type = type; }
+
+    public String getNote() { return note; }
+    public void setNote(String note) { this.note = note; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Lesson lesson)) return false;
+        return Objects.equals(getId(), lesson.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
